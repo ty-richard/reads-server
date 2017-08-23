c9c0176a8d3ad6b25ac1c19066e59b7b33c8c671
Ty Richard <tyrichard2@icloud.com>
2017-08-23 16:42:52 -0600

    updates

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
@@ -6,4 +7,8 @@ module.exports = {
         client: 'pg',
         connection: 'postgres://localhost/reads'
     },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
+    }
 };
